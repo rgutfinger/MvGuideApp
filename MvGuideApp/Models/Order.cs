36f510574c4ad74df14f7fd452c176d96ad06c44
rgutfinger <gutfinger@cox.net>
2018-07-31 09:33:37 -0700

    Add project files.

---
+++ MvGuideApp/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvGuideApp.Models
+{
+	public class Order
+	{
+		public string OrderID { get; set; }
+		public string Desc { get; set; }
+		public DateTime DeliveryDate { get; set; }
+
+		public Order(string id, string desc)
+		{
+			OrderID = id;
+			Desc = desc;
+			DeliveryDate = DateTime.Now;
+		}
+	}
+}
---
+++ MvGuideApp/ViewModels/OrderVM.cs
@@ -0,0 +1,20 @@
+﻿using MvGuideApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvGuideApp.ViewModels
+{
+	public class OrderVM
+	{
+		static List<Order> s_orders=new List<Order>();
+
+		public OrderVM()
+		{
+			s_orders.Add(new Order("1", "test 1"));
+			s_orders.Add(new Order("2", "test 2"));
+		}
+	}
+}
